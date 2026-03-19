063ccdde00262f92cdb90a559e3776a48d2cbecd
John Naumann <3016847+johnnaumann@users.noreply.github.com>
2026-03-19 13:03:10 +0100

    Refine header and hero components for enhanced responsiveness and layout
    - Adjusted padding and dimensions in header.js for better mobile compatibility.
    - Updated hero.js to improve section height, padding, and text presentation for various screen sizes.
    - Incorporated a new Trustpilot mobile image and refined element visibility for improved user experience.

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,13 @@ import hamburgerUrl from '/img/hamburger.png';
 
 export function renderHeader() {
   return `
-    <header class="fixed top-0 left-0 right-0 flex justify-center items-center w-full p-[35px] z-10 overflow-hidden">
+    <header class="fixed top-0 left-0 right-0 flex justify-center items-center w-full md:p-[35px] p-[20px] z-10 overflow-hidden">
       <div class="flex justify-between items-center w-[1220px]">
         <a href="#hero">
           <img
             src="${logoUrl}"
             alt="Strathclyde Domestic Roofing logo"
-            class="block h-[52px] w-[293px] object-contain"
+            class="block w-[164px] h-[30px] md:h-[52px] md:w-[293px] object-contain"
           />
         </a>
         <div class="flex items-center">
@@ -19,7 +19,7 @@ export function renderHeader() {
             <a href="#pricing" class="hover:text-primary transition-colors ml-[34px]">Pricing</a>
             <a href="#faq" class="hover:text-primary transition-colors ml-[34px]">FAQ</a>
           </nav>
-          <a href="#pricing" class="ml-[34px] text-white tracking-[-0.3px] inline-flex items-center justify-center rounded-full bg-primary w-[180px] h-[50px] transition-colors">
+          <a href="#pricing" class="ml-[34px] text-white tracking-[-0.3px] hidden md:inline-flex items-center justify-center rounded-full bg-primary w-[180px] h-[50px] transition-colors">
             Get started
           </a>
           <img src="${hamburgerUrl}" alt="Hamburger menu" class="block lg:hidden w-[24px] h-[24px] ml-[24px]">
--- src/components/hero.js
+++ src/components/hero.js
@@ -3,50 +3,52 @@ import listIconUrl from '/img/list.png';
 import trustmarkUrl from '/img/trustmark.png';
 import heroImageUrl from '/img/hero-image.png';
 import trustpilotUrl from '/img/trustpilot.png';
+import trustpilotMobileUrl from '/img/trustpilot-mobile.png';
 
 export function renderHero() {
   return `
-    <section id="hero" class="h-[720px] w-full relative overflow-hidden">
-      <div class="absolute top-0 left-0 w-full h-[720px] px-[35px]">
-        <div class="mx-auto flex h-[720px] w-[1220px] relative">
-          <div class="w-[1202px] h-[1054px] absolute top-[-95px] lg:right-[-325px] right-[-165px] rotate-[5deg]">
+    <section id="hero" class="md:h-[720px] h-[600px] w-full relative overflow-hidden">
+      <div class="absolute top-0 left-0 w-full md:h-[720px] h-[600px] md:px-[35px] px-[20px]">
+        <div class="mx-auto flex md:h-[720px] h-[600px] w-[1220px] relative">
+          <div class="w-[1202px] h-[1054px] absolute top-[-95px] lg:right-[-325px] md:right-[-165px] right-[-50px] rotate-[5deg]">
             <img src="${heroPatternUrl}" alt="Hero image" class="absolute top-0 left-0 w-full h-full z-0">
           </div>
         </div>
       </div>
 
-      <div class="absolute top-0 left-0 right-0 w-full h-[720px]">
-        <div class="absolute bottom-0 left-0 w-full h-[100px] bg-white p-[35px]">
+      <div class="absolute top-0 left-0 right-0 w-full md:h-[720px] h-[600px]">
+        <div class="absolute bottom-0 left-0 w-full h-[100px] bg-white md:p-[35px] p-[20px]">
           <div class="mx-auto flex w-[1220px] h-full items-center">
-            <img src="${trustpilotUrl}" alt="" class="w-[400px] h-[24px]">
+            <img src="${trustpilotUrl}" alt="" class="w-[400px] h-[24px] md:block hidden">
+            <img src="${trustpilotMobileUrl}" alt="" class="w-[260px] h-[20px] md:hidden block">
           </div>
         </div>
       </div>
 
-      <div class="absolute top-0 left-0 w-full h-[720px] px-[35px]">
-        <div class="mx-auto flex h-[720px] w-[1220px] relative">
-          <div class="lg:mt-[200px] mt-[180px]">
-            <h2 class="text-white text-[40px] leading-[51px] font-bold">Certified and trusted roofing<br>specialists in Glasgow</h2>
-            <ul class="text-[#D2D2D2] text-[18px] font-medium mt-[30px] font-title mb-[30px]">
+      <div class="absolute top-0 left-0 w-full md:h-[720px] h-[600px] md:px-[35px] px-[20px]">
+        <div class="mx-auto flex md:h-[720px] h-[600px] w-[1220px] relative">
+          <div class="lg:mt-[200px] md:mt-[180px] mt-[110px]">
+            <h2 class="text-white text-[27px] md:text-[40px] leading-[34px] md:leading-[51px] w-[390px] md:w-[650px] font-bold balance-text">Certified and trusted roofing specialists <br class="md:hidden">in Glasgow</h2>
+            <ul class="text-[#D2D2D2] md:text-[18px] text-[15px] font-medium md:mt-[30px] mt-[35px] font-title mb-[37.5px]">
               <li class="flex items-center mb-[8px]">
-                <img src="${listIconUrl}" class="w-[24px] h-[24px] inline-block mr-[15px]"> NFRC &amp; Trustmark Certified
+                <img src="${listIconUrl}" class="md:w-[24px] w-[18px] h-[18px] md:h-[24px] inline-block md:mr-[15px] mr-[10px]"> NFRC &amp; Trustmark Certified
               </li>
               <li class="flex items-center mb-[8px]">
-                <img src="${listIconUrl}" class="w-[24px] h-[24px] inline-block mr-[15px]"> Scottish Home Improvement Awards finalist 2023/24/25
+                <img src="${listIconUrl}" class="md:w-[24px] w-[18px] h-[18px] md:h-[24px] inline-block md:mr-[15px] mr-[10px]"> <span class="md:block hidden">Scottish Home Improvement</span><span class="md:hidden block">SHI</span>&nbsp;Awards finalist 2023/24/25</span>
               </li>
               <li class="flex items-center">
-                <img src="${listIconUrl}" class="w-[24px] h-[24px] inline-block mr-[15px]"> 20 year warranty on all new roofs
+                <img src="${listIconUrl}" class="md:w-[24px] w-[18px] h-[18px] md:h-[24px] inline-block md:mr-[15px] mr-[10px]"> 20 year warranty on all new roofs
               </li>
             </ul>
             <div class="flex items-center mb-[30px]">
-              <button class="bg-[#17BADC] rounded-full text-white font-medium text-[20px] w-[350px] h-[60px]">Book your free survey and quote</button>
-              <img src="${trustmarkUrl}" alt="" class="block w-[80px] h-[50px] ml-[30px]">
+              <button class="bg-[#17BADC] rounded-full text-white font-medium md:text-[20px] text-[14px] md:w-[350px] w-[233px] h-[50px] md:h-[60px]">Book your free survey and quote</button>
+              <img src="${trustmarkUrl}" alt="" class="block md:w-[80px] w-[67px] md:h-[50px] h-[42px] md:ml-[30px] ml-[20px]">
             </div>
-            <div class="flex items-center pl-[29px]">
-              <span class="text-[#D2D2D2] text-[15px] font-medium">Prefer to call?</span><a href="tel:+448008008000" class="text-[#45D1EF] text-[20px] font-title ml-[2px] font-semibold">0800 800 8000</a>
+            <div class="flex items-center md:pl-[29px]">
+              <span class="text-[#D2D2D2] md:text-[15px] text-[14px] font-medium">Prefer to call?</span><a href="tel:+448008008000" class="text-[#45D1EF] md:text-[20px] text-[16px] font-title md:ml-[2px] ml-[6px] font-semibold">0800 800 8000</a>
             </div>
           </div>
-          <div class="w-[1202px] h-[1054px] absolute top-[-95px] lg:right-[-325px] right-[-165px] rotate-[5deg]">
+          <div class="w-[1202px] h-[1054px] absolute top-[-95px] lg:right-[-325px] right-[-165px] rotate-[5deg] hidden md:block">
             <img src="${heroImageUrl}" alt="Hero image" class="absolute top-[278px] left-[350px] block w-[549.5px] h-[530.5px] relative z-10">
           </div>
         </div>
