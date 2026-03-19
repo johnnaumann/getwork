cd90daa55190ebe360441de621e8d773a548f2c9
John Naumann <3016847+johnnaumann@users.noreply.github.com>
2026-03-19 17:32:26 +0100

    Enhance header component hover effects for improved user experience
    - Updated hover styles for the header and navigation links to ensure consistent behavior across different screen sizes.
    - Adjusted class names to apply hover effects specifically for larger devices, enhancing interactivity and visual feedback.

--- src/components/header.js
+++ src/components/header.js
@@ -3,15 +3,15 @@ import hamburgerUrl from '/img/hamburger.png';
 
 export function renderHeader() {
   return `
-    <header class="fixed top-0 left-0 right-0 flex justify-center items-center w-full md:p-[35px] p-[20px] z-10 overflow-hidden transition-colors group hover:bg-background">
+    <header class="fixed top-0 left-0 right-0 flex justify-center items-center w-full md:p-[35px] p-[20px] z-10 overflow-hidden transition-colors group lg:hover:bg-background">
       <div class="flex justify-between items-center w-full max-w-[1240px] flex-nowrap">
-        <a href="#hero" class="group-hover:[--fill-0:#000000]">
+        <a href="#hero" class="lg:group-hover:[--fill-0:#000000]">
           <span class="block w-[164px] h-[30px] md:h-[52px] md:w-[293px] shrink-0">
             ${logoSvg}
           </span>
         </a>
         <div class="flex items-center flex-nowrap">
-          <nav class="hidden lg:flex text-white text-[15px] tracking-[-0.3px] transition-colors group-hover:text-black flex-nowrap items-center whitespace-nowrap gap-[clamp(16px,2.5vw,34px)]">
+          <nav class="hidden lg:flex text-white text-[15px] tracking-[-0.3px] transition-colors lg:group-hover:text-black flex-nowrap items-center whitespace-nowrap gap-[clamp(16px,2.5vw,34px)]">
             <a href="#features" class="transition-colors">Home</a>
             <a href="#social-proof" class="transition-colors">Services</a>
             <a href="#pricing" class="transition-colors">Why us?</a>
@@ -22,7 +22,7 @@ export function renderHeader() {
           <a href="#pricing" class="hover:bg-[#2272B6] ml-[34px] text-white tracking-[-0.3px] hidden md:inline-flex items-center justify-center rounded-full bg-primary w-[180px] h-[50px] transition-colors">
             Get a free quote
           </a>
-          <img src="${hamburgerUrl}" alt="Hamburger menu" class="block lg:hidden w-[24px] h-[24px] ml-[24px] transition group-hover:invert">
+          <img src="${hamburgerUrl}" alt="Hamburger menu" class="block lg:hidden w-[24px] h-[24px] ml-[24px] transition lg:group-hover:invert">
         </div>
       </div>
     </header>
