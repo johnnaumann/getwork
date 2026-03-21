7f49dedc0278b2a0f41e37fe631154038558ab21
John Naumann <3016847+johnnaumann@users.noreply.github.com>
2026-03-21 08:49:37 +0100

    Update Trustpilot widget template ID for improved integration

--- src/components/trustpilot-widget.js
+++ src/components/trustpilot-widget.js
@@ -9,7 +9,7 @@ export function renderTrustpilotWidget() {
     <div
       class="trustpilot-widget"
       data-locale="en-GB"
-      data-template-id="54194556b3550309ad296228"
+      data-template-id="5419b6ffb0d04a076446a9af"
       data-businessunit-id="671f978ec65a99f755dbdc62"
       data-style-height="20px"
       data-style-width="100%"
